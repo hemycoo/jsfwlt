4cb5573909ae6777023aee165867d0b69b7d974f
hemycoo <1063854432@qq.com>
2019-11-18 10:53:30 +0800

    全局框架0.1

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.el-container {
+  height: 100%;
+  width: 100%;
+}
+.el-header,
+.el-main,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import App from '../App.vue'
+import Home from '../components/Home'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/app',
+    name: 'app',
+    component: App
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/home',
+    name: 'home',
+    component: Home
   }
 ]
 
